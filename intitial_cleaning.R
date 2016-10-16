15e5e57ea40277867cf32069b77c0f5bced8d2b6
Ryan Burge <ryanburge@gmail.com>
2016-10-16 16:29:34 -0500

    Getting my filters right.

--- intitial_cleaning.R
+++ intitial_cleaning.R
@@ -1,3 +1,23 @@
 gss <- read_dta("D:/GSS_reltrad.dta", encoding = NULL)
 small_gss <- select(gss, year, evangelical, mainline, catholic, blackprot, jewish, otherfaith, nofaith, reltrad, partyid)
+small_gss$partyid[small_gss$partyid == 7] <- NA
+
 write.csv(small_gss, "raphael.csv")
+
+gss72 <- filter(small_gss, year==1972)
+crosstab(gss72$evangelical, gss72$partyid)
+crosstab(gss72$mainline, gss72$partyid)
+crosstab(gss72$blackprot, gss72$partyid)
+crosstab(gss72$catholic, gss72$partyid)
+crosstab(gss72$jewish, gss72$partyid)
+crosstab(gss72$otherfaith, gss72$partyid)
+crosstab(gss72$nofaith, gss72$partyid)
+
+test$partyid <- factor(test$partyid, levels=unique(test$partyid))
+test %>%  filter(reltrad == "evangelical") %>% ggplot(aes(x= partyid, y=count)) + geom_bar(stat = "identity")
+
+small_gss$partyid <- factor(small_gss$partyid, levels = c(0,1,2,3,4,5, 6),labels = c("strong democrat", "moderate democrat", "lean democrat", "independent","lean republican", "moderate republican", "strong republican"))
+small_gss <- na.omit(small_gss)
+
+
+small_gss %>%  filter(mainline == 1 & year == 1972) %>% ggplot(aes(x= partyid, fill=factor(partyid)), color= factor(partyid)) + geom_bar() + theme(axis.text.x = element_text(angle = 90)) + scale_fill_brewer(palette="RdBu", direction=-1)
