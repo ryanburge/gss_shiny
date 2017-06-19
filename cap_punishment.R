ef9b431af05e7f8b94bf252089f9f00258c0be26
Ryan Burge <ryanburge@gmail.com>
2017-06-19 06:38:27 -0500

    added a bunch of stuff to crime

--- cap_punishment.R
+++ cap_punishment.R
@@ -1,11 +1,17 @@
+## Death Penalty
+
 cap <- gss %>% select(year, bible, reltrad, partyid, cappun) %>% na.omit()
 
 cap$cappun <- Recode(cap$cappun, "1=1; else=0")
 
 
-death <- cap %>%  group_by(year, reltrad) %>% count(cappun) %>% mutate(pct = prop.table(n)) %>%  filter(cappun ==1) %>% select(year, pct)
+death <- cap %>%  group_by(year, reltrad) %>% 
+  count(cappun) %>% mutate(pct = prop.table(n)) %>%  
+  filter(cappun ==1) %>% select(year, pct)
 
-d1 <- cap %>%  group_by(year) %>% count(cappun) %>% mutate(pct = prop.table(n)) %>%  filter(cappun ==1) %>% select(year, pct)
+d1 <- cap %>%  group_by(year) %>% 
+  count(cappun) %>% mutate(pct = prop.table(n)) %>% 
+  filter(cappun ==1) %>% select(year, pct)
 
 
 death$reltrad <- Recode(death$reltrad, "1='Evangelical Protestants';
@@ -22,5 +28,72 @@ ggplot(death, aes(x=year, y=pct*100, color = reltrad, label = reltrad)) +
   theme(plot.title = element_text(hjust = 0.5)) +
   theme(text=element_text(size=24, family="KerkisSans")) + labs(x= "Year", y = "% in Favor", title = "Support for the Death Penalty ", caption = "Data from the GSS (1984-2016)")+
   geom_smooth(se = FALSE) + 
-  scale_x_continuous(breaks = seq(min(lit$year), max(lit$year), by = 2)) +
+  scale_x_continuous(breaks = seq(min(death$year), max(death$year), by = 2)) +
   scale_color_manual(values = c("#F8766D", "#CD9600", "darkmagenta", "#00BE67","yellow", "turquoise", "violet", "black"))
+
+
+## Crime Spending 
+
+
+crime <- gss %>% select(year, bible, reltrad, partyid, natcrime) %>% na.omit()
+
+crime$natcrime <- Recode(crime$natcrime, "1=1; else=0")
+
+
+crim <- crime %>%  group_by(year, reltrad) %>% 
+  count(natcrime) %>% mutate(pct = prop.table(n)) %>%  
+  filter(natcrime ==1) %>% select(year, pct)
+
+crim1 <- crime %>%  group_by(year) %>% 
+  count(natcrime) %>% mutate(pct = prop.table(n)) %>%  
+  filter(natcrime ==1) %>% select(year, pct) %>% mutate(reltrad = c("Entire Sample")) %>% rbind(crim)
+
+crim$reltrad <- Recode(crim$reltrad, "1='Evangelical Protestants';
+                       2='Mainline Protestants';
+                        3='Black Protestants';
+                        4='Catholic'; 
+                        5='Jewish';
+                        6= 'Other Faith';
+                        7= 'No Faith'", as.factor = TRUE)
+
+ggplot(crim1, aes(x=year, y=pct*100, color = reltrad, label = reltrad)) +  
+  theme(legend.position="bottom") + theme(legend.title = element_blank()) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(text=element_text(size=24, family="KerkisSans")) + labs(x= "Year", y = "% Saying Too Little", title = "The US is Spending Too Little on Halting the Rising Crime Rate ", caption = "Data from the GSS (1984-2016)")+
+  geom_smooth(se = FALSE) + 
+  scale_x_continuous(breaks = seq(min(crim$year), max(crim$year), by = 2)) +
+  scale_color_manual(values = c("#F8766D", "#CD9600","black", "darkmagenta", "#00BE67","yellow", "turquoise", "violet"))
+
+
+## Drug Spending
+
+drugs <- gss %>% select(year, bible, reltrad, partyid, natdrug) %>% na.omit()
+
+drugs$natdrug <- Recode(drugs$natdrug, "1=1; else=0")
+
+
+d1 <- drugs %>%  group_by(year, reltrad) %>% 
+  count(natdrug) %>% mutate(pct = prop.table(n)) %>%  
+  filter(natdrug ==1) %>% select(year, pct)
+
+d1$reltrad <- Recode(d1$reltrad, "1='Evangelical Protestants';
+                       2='Mainline Protestants';
+                     3='Black Protestants';
+                     4='Catholic'; 
+                     5='Jewish';
+                     6= 'Other Faith';
+                     7= 'No Faith'", as.factor = TRUE)
+
+d2 <- drugs %>%  group_by(year) %>% 
+  count(natdrug) %>% mutate(pct = prop.table(n)) %>%  
+  filter(natdrug ==1) %>% select(year, pct) %>% mutate(reltrad = c("Entire Sample")) %>% rbind(d1)
+
+
+
+ggplot(d2, aes(x=year, y=pct*100, color = reltrad, label = reltrad)) +  
+  theme(legend.position="bottom") + theme(legend.title = element_blank()) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(text=element_text(size=24, family="KerkisSans")) + labs(x= "Year", y = "% Saying Too Little", title = "The US is Spending Too Little on Dealing with Drug Addiction ", caption = "Data from the GSS (1984-2016)")+
+  geom_smooth(se = FALSE) + 
+  scale_x_continuous(breaks = seq(min(crim$year), max(crim$year), by = 2)) +
+  scale_color_manual(values = c("#F8766D", "#CD9600","black", "darkmagenta", "#00BE67","yellow", "turquoise", "violet"))
--- poq_long_chart.R
+++ poq_long_chart.R
@@ -1,45 +1,36 @@
-gss <- read_dta("D:/GSS_reltrad.dta", encoding = NULL)
-gss16 <- read_dta("D:/cces/data/gss16.dta")
+library(haven)
+library(dplyr)
+library(ggplot2)
+library(car)
+library(extrafont)
 
+gss <- read_dta("C:/Users/Ryan Burge/Desktop/gss_reltrad.dta")
 
 gss$bagain <- Recode(gss$reborn, "1=1;else=0")
 gss$white <- Recode(gss$race, "1=1;else=0")
 gss$protestant <- Recode(gss$relig, "1=1;else=0")
 
-
 gss$whtevan <- gss$evangelical + gss$white
 gss$whtbaprot <- gss$bagain + gss$white + gss$protestant
 
-whtevan <- gss %>% filter(whtevan ==2 & year >=2004) %>% group_by(year) %>% summarise(mean = mean(partyid, na.rm=TRUE))
-whtbaprot <- gss %>% filter(whtbaprot ==3 & year >=2004) %>% group_by(year) %>% summarise(mean = mean(partyid, na.rm=TRUE))
-whtevan$label <- c("White Evangelical Protestant")
-whtbaprot$label <- c("White BA + Prot.")
+whtevan <- gss %>% filter(whtevan ==2 & year >=2004) %>% 
+  group_by(year) %>% summarise(mean = mean(partyid, na.rm=TRUE)) %>% 
+  mutate(label = c("White Evangelical Protestant"))
 
+whtbaprot <- gss %>% filter(whtbaprot ==3 & year >=2004) %>% 
+  group_by(year) %>% summarise(mean = mean(partyid, na.rm=TRUE)) %>% 
+  mutate(label = c("white BA + Prot."))
 
-gss16$bagain <- Recode(gss16$reborn, "1=1;else=0")
-gss16$white <- Recode(gss16$race, "1=1;else=0")
-gss16$protestant <- Recode(gss16$relig, "1=1;else=0")
-
-
-gss16$whtevan <- gss16$evangelical + gss16$white
-gss16$whtbaprot <- gss16$bagain + gss16$white + gss16$protestant
-
-whtevan16 <- gss16 %>% filter(whtevan ==2 & year >=2004) %>% group_by(year) %>% summarise(mean = mean(partyid, na.rm=TRUE))
-whtbaprot16 <- gss16 %>% filter(whtbaprot ==3 & year >=2004) %>% group_by(year) %>% summarise(mean = mean(partyid, na.rm=TRUE))
-whtevan16$label <- c("White Evangelical Protestant")
-whtbaprot16$label <- c("White BA + Prot.")
-
-pid <- rbind(whtbaprot, whtevan, whtbaprot16, whtevan16)
-
-
+pid <- rbind(whtevan, whtbaprot)
 
 ggplot(pid, aes(x=year, y=mean, color = label, label = label)) + geom_line(aes(group=label), size = 1.5) + 
   coord_flip() + scale_y_continuous(limits = c(0,6), breaks = c(0,1,2,3,4,5,6), labels = c("Strong Democrat", "Not Strong Democrat", "Ind., Near Dem.", "Independent", "Lean Republican", "Moderate Republican", "Strong Republican")) +
   theme(legend.title = element_blank()) +
   theme(legend.position="bottom") +
   theme(plot.title = element_text(hjust = 0.5)) +
-  theme(text=element_text(size=14, family="KerkisSans")) + labs(x= "Year", y = "Party Identification", title = "How has Partisanship Shifted Over Time?", caption = "Data from the GSS (1972-2016)")+
-  scale_color_manual(values = c("black", "gray", "black", "#7CAE00", "#00BE67","#00BFC4", "#00A9FF", "#C77CFF", "#FF61CC"))
+  theme(text=element_text(size=20, family="KerkisSans")) + 
+  labs(x= "Year", y = "Party Identification", title = "How has Partisanship Shifted Over Time?", caption = "Data from the GSS (2004-2016)")+
+  scale_color_manual(values = c("black", "gray", "black", "#7CAE00", "#00BE67","#00BFC4", "#00A9FF", "#C77CFF", "#FF61CC")) 
 
-ggsave(file="long_compare_poq.png", type = "cairo-png", width = 9, height = 10)
+ggsave(file="long_compare_poq.png", type = "cairo-png", width = 15, height = 10)
 
